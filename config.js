d23f89d137d037947e5e3fe3ebb22e4dbe6b7ef6
idutsu <email@idutsu.com>
2024-01-07 17:57:00 +0900

    edit index.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-export const REIBUN_CSV_PATH = 'csv/reibun.csv';
-export const NOUN_CSV_PATH = 'csv/noun.csv';
-export const PART_CSV_PATH = 'csv/part.csv';
-export const VERB_CSV_PATH = 'csv/verb.csv';
+export const REIBUN_CSV_PATH = 'csv/reibun.csv'; // reibun-dev.csv
+export const NOUN_CSV_PATH = 'csv/noun.csv'; // noun-dev.csv
+export const PART_CSV_PATH = 'csv/part.csv'; // part-dev.csv
+export const VERB_CSV_PATH = 'csv/verb.csv'; // verb-dev.csv
 export const API_POST_SAVE = '/save';
 export const API_GET_PART = '/part';
 export const API_GET_NOUN = '/noun';
